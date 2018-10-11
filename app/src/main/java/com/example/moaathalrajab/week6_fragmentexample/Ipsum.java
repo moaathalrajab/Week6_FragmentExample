1b55e65a8142e7bfdc5b5c28134799807721c857
Moaath Alrajab <moaath@alrajab.net>
2018-10-11 01:09:49 -0400

    Initial commit

---
+++ app/src/main/java/com/example/moaathalrajab/week6_fragmentexample/Ipsum.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (C) 2012 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.moaathalrajab.week6_fragmentexample;
+
+public class Ipsum {
+
+    static String[] Headlines = {
+        "Article One",
+        "Article Two"
+    };
+
+    static String[] Articles = {
+        "Article One\n\nPossibility is something we used to describe an event that may or may not happen. And it might not be always possible to calculate how likely the event is to occur. Possibility is the qualitative characteristic of an event. ",
+            "Article Two\n\nProbability of an event is the likelihood or chance with which that event could occur or happen. Probability is basically the numerical characteristic of likelihood of an event."   };
+}
+
